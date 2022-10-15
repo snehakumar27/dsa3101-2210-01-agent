622594f0e42dcebbce1a53ed43fda1695ea66a96
AijiaZ <zhangaijia.0627@gmail.com>
2022-10-15 23:25:01 +0800

    added template for dashboard

---
+++ graph-of-map/version2.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import plotly.express as px
+import dash
+from dash import html
+from dash import dcc
+from dash.dependencies import Input, Output
+from PIL import Image
+
+#### LOADING DATASETS ####
+light_density = pd.read_csv("light_density.csv")
+roads_density = pd.read_csv("road_density.csv")
+##########################
+
+
+######FUNCTIONS IF NEEDED #########
+
+#==========BOHAN================
+
+
+
+
+##============================##
+
+#==========TINGXUAN==============
+
+
+
+
+##============================##
+
+#==========ANDREW================
+
+
+
+
+##============================##
+
+#==========AIJIA================
+
+
+
+
+##============================##
+
+
+
+
+#### BUILDING APP ####
+app = dash.Dash()
+app.layout = html.Div(
+    children=[
+        #### HTML DIV TO add time slider (AIJIA)####
+        html.Div(children = [
+        ], style = {}),
+        #### END DIV 1 ####
+
+        #### HTML DIV TO add graph (AIJIA)####
+        html.Div(children = [
+        ], style = {}),
+        #### END DIV 2 ####
+
+        #### HTML DIV TO add INPUT for lanes (TINGXUAN)####
+        html.Div(children = [
+        ], style = {}),
+        #### END DIV 3 ####
+
+        #### HTML DIV TO add INPUT for traffic light waiting time (BOHAN)####
+        html.Div(children = [
+        ], style = {}),
+        #### END DIV 4 ####
+
+        #### HTML DIV TO add INPUT for Population Density (BOHAN)####
+        html.Div(children = [
+        ], style = {}),
+        #### END DIV 5 ####
+
+        #### HTML DIV TO add OUTPUT for CARS (ANDREW)####
+        html.Div(children = [
+        ], style = {}),
+        #### END DIV 6 ####
+
+        #### HTML DIV TO add OUTPUT for PEDESTRIANS (ANDREW)####
+        html.Div(children = [
+        ], style = {}),
+        #### END DIV 7 ####
+
+    ]
+
+)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port = 8055)
